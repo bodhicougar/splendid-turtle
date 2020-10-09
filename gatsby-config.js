fc77b9459abd6769bf40aa55cababe32fef745f7
bodhicougar <info@blackboxmd.fyi>
2020-10-09 10:21:52 +0200

    Hotjar tracking code korrigiert

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-hubspot',
       options: {
-        trackingCode: '7395504',
+        trackingCode: '2018853',
         respectDNT: true,
         productionOnly: true,
       },
